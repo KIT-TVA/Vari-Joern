d8607bf78f57d190aaeb5e5a82c3b95055ab476d
Erik Hofmayer <ehofmayer@outlook.de>
2023-09-07 19:12:20 +0200

    Restructure Joern output to include filename, print finding locations in summary

--- src/main/java/edu/kit/varijoern/analyzers/joern/JoernFinding.java
+++ src/main/java/edu/kit/varijoern/analyzers/joern/JoernFinding.java
@@ -4,29 +4,75 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Contains information about a finding Joern found.
  */
 public class JoernFinding {
-    private static final String TITLE_KEY = "title";
-    private static final String SCORE_KEY = "score";
+    private final String name;
+    private final String title;
+    private final String description;
+    private final double score;
     private final List<JoernEvidence> evidence;
-    private final KeyValuePairs keyValuePairs;
 
     /**
      * Creates a new {@link JoernFinding} containing the specified information.
      *
-     * @param evidence      information about the source that caused this finding
-     * @param keyValuePairs additional information about this finding
+     * @param name        the name of the query that caused this finding
+     * @param title       the title of the finding
+     * @param description a description of the problem
+     * @param score       the score of the finding
+     * @param evidence    information about the source that caused this finding
      */
     @JsonCreator
     public JoernFinding(
-        @JsonProperty("evidence") List<JoernEvidence> evidence,
-        @JsonProperty("keyValuePairs") List<KeyValuePair> keyValuePairs) {
+        @JsonProperty("name") String name,
+        @JsonProperty("title") String title,
+        @JsonProperty("description") String description,
+        @JsonProperty("score") double score,
+        @JsonProperty("evidence") List<JoernEvidence> evidence) {
+        this.name = name;
+        this.title = title;
+        this.description = description;
+        this.score = score;
         this.evidence = evidence;
-        this.keyValuePairs = new KeyValuePairs(keyValuePairs);
+    }
+
+    /**
+     * Returns the name of the query that caused this finding.
+     *
+     * @return the name of the query
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the title of the finding.
+     *
+     * @return the title of the finding
+     */
+    public String getTitle() {
+        return this.title;
+    }
+
+    /**
+     * Returns a description of the problem.
+     *
+     * @return a description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Returns the score of this finding.
+     *
+     * @return the score of this finding
+     */
+    public double getScore() {
+        return this.score;
     }
 
     /**
@@ -38,44 +84,11 @@ public class JoernFinding {
         return evidence;
     }
 
-    /**
-     * Returns additional data about this finding.
-     *
-     * @return addition data
-     */
-    public KeyValuePairs getKeyValuePairs() {
-        return keyValuePairs;
-    }
-
-    /**
-     * Returns the title of the finding if specified.
-     *
-     * @return the title of the finding
-     */
-    public Optional<String> getTitle() {
-        return Optional.ofNullable(this.keyValuePairs.get(TITLE_KEY));
-    }
-
-    /**
-     * Returns the score of this finding if specified.
-     *
-     * @return the score of this finding
-     */
-    public Optional<Float> getScore() {
-        String scoreString = this.keyValuePairs.get(SCORE_KEY);
-        if (scoreString == null)
-            return Optional.empty();
-        return Optional.of(Float.parseFloat(scoreString));
-    }
-
     @Override
     public String toString() {
-        String title = this.getTitle().orElse("(no title)");
-        String score = this.getScore().map(String::valueOf).orElse("(no score)");
-        StringBuilder sb = new StringBuilder();
-        sb.append(title);
-        sb.append(": ");
-        sb.append(score);
-        return sb.toString();
+        return "%s: %s at %s".formatted(this.title,
+            this.score,
+            this.evidence.stream().map(JoernEvidence::toString).collect(Collectors.joining(", "))
+        );
     }
 }
