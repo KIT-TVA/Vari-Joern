1bfe524d4674a205f4e256704ec02683c3cd4663
Tim Baechle <t.baechle1999@googlemail.com>
2024-07-11 23:28:17 +0200

    Clean up PresenceConditionDemo.py.

--- src/main/python/sugarlyzer/PresenceConditionDemo.py
+++ src/main/python/sugarlyzer/PresenceConditionDemo.py
@@ -1,15 +1,24 @@
+import argparse
+
 from python.sugarlyzer.SugarCRunner import process_alarms
-from python.sugarlyzer.models.InferAlarm import InferAlarm
+from python.sugarlyzer.models.Alarm import Alarm
 from pathlib import Path
 
-desugaredFilePath = Path("/home/tim/Desktop/MA_Workspace/sample-spls/SimpleCode/SimpleCodeVariabilityBugComplexDesugared.c")
-lineNumber = 1298
-alarm = InferAlarm(input_file = desugaredFilePath,
-                   line_in_input_file = lineNumber,
-                   bug_type = "({cpg.method(\"(?i)(__strcpy.*|strncpy)\").callIn}).l",
-                   message = "__strcpy_976  ( __dest_1122 , __src_1121 )",
-                   alarm_type = "Pattern Occurance",
-                   warning_path = [lineNumber])
+# Example call: python PresenceConditionDemo.py
+# /home/tim/Desktop/MA_Workspace/sample-spls/SimpleCode/SimpleCodeVariabilityBugComplexDesugared.c 1298
+
+argumentParser = argparse.ArgumentParser(description="A simple demo script determining the presence condition for "
+                                                     "an arbitrary line of code in a source code file desugared by "
+                                                     "SugarC.")
+argumentParser.add_argument("path", help="The full path to the desugared file.", type=str)
+argumentParser.add_argument("loc", help="The line of code for which the presence condition should be determined.",
+                            type=int)
+arguments = argumentParser.parse_args()
+
+desugaredFilePath = Path(arguments.path)
+lineNumber = arguments.loc
+alarm = Alarm(input_file=desugaredFilePath,
+              line_in_input_file=lineNumber)
 
 print("Starting to process alarms...")
 processedAlarms = process_alarms([alarm], desugaredFilePath)
