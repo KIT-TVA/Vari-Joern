e5c3d8264ce40a9c36df32cdf9f61350dab7bde6
Tim Baechle <t.baechle1999@googlemail.com>
2025-01-23 08:33:45 +0000

    Complete parse_make_output of FiascoSpecification.py.

--- src/main/python/sugarlyzer/models/program/FiascoSpecification.py
+++ src/main/python/sugarlyzer/models/program/FiascoSpecification.py
@@ -1,4 +1,5 @@
 import os
+import re
 import subprocess
 from pathlib import Path
 from typing import List, Dict, Callable
@@ -18,40 +19,51 @@ class FiascoSpecification(ProgramSpecification):
 
         with open(make_output_file, "r") as make_output:
             compile_call: bool = False
-            file_pattern: str | None = None
+            file_path: str | None = None
+            included_files = []
+            included_dirs = []
 
             for line in make_output:
                 line: str = line.strip()
 
-                if line.startswith("x86_64-linux-gnu"):
+                if "Entering directory" in line:
+                    # Directory switch
+                    current_building_directory = line.split("'")[1]
+                elif line.startswith("x86_64-linux-gnu"):
+                    # Compile call.
                     compile_call = True
+                    # Reset data collected for compile call.
+                    file_path = None
+                    included_files = []
+                    included_dirs = []
 
-                    # file_name_match = re.search(r' (\S+\.c)(\s+|$)', line)
-                    # if file_name_match is not None:
-                    #     relative_file_path = file_name_match.group(1)
-                    #
-                    #     # Resolve full paths of the included files.
-                    #     included_files = []
-                    #     for included_file in re.findall(r'-include ?\S+', line):
-                    #         included_file = included_file.lstrip("-include").strip()
-                    #         full_file_path = (Path(building_directory) / Path(included_file)).resolve()
-                    #         included_files.append(full_file_path)
-                    #
-                    #     # Resolve full paths of the included dirs.
-                    #     included_dirs = []
-                    #     for included_dir in re.findall(r'-I ?\S+', line):
-                    #         included_dir = included_dir.lstrip("-I").strip()
-                    #         full_dir_path = (Path(building_directory) / Path(included_dir)).resolve()
-                    #         included_dirs.append(full_dir_path)
-                    #
-                    #     make_entry = {'file_pattern': relative_file_path.replace('.', r'\.') + '$',
-                    #                   'included_files': included_files,
-                    #                   'included_directories': included_dirs,
-                    #                   'build_location': building_directory}
-                    #     includes_per_file_pattern.append(make_entry)
+                if compile_call:
+                    # Resolve name of the file that is about to be compiled.
+                    file_name_match = re.search(r' (\S+\.c)(\s+|$)', line)
+                    if file_name_match is not None:
+                        file_path = file_name_match.group(1)
 
-                if not line or not line.endswith("\\"):
-                    compile_call = False
+                    # Resolve full paths of the included files.
+                    for included_file in re.findall(r'-include ?\S+', line):
+                        included_file = included_file.lstrip("-include").strip()
+                        full_file_path = (Path(building_directory) / Path(included_file)).resolve()
+                        included_files.append(full_file_path)
+
+                    # Resolve full paths of the included dirs.
+                    for included_dir in re.findall(r'-I ?\S+', line):
+                        included_dir = included_dir.lstrip("-I").strip()
+                        full_dir_path = (Path(building_directory) / Path(included_dir)).resolve()
+                        included_dirs.append(full_dir_path)
+
+                    if not line or not line.endswith("\\"):
+                        compile_call = False
+                        make_entry = {'file_pattern': file_path.replace('.', r'\.') + '$',
+                                      'included_files': included_files,
+                                      'included_directories': included_dirs,
+                                      'build_location': building_directory}
+                        includes_per_file_pattern.append(make_entry)
+
+
 
         return includes_per_file_pattern
 
