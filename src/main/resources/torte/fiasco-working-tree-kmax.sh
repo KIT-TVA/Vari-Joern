8e0d97f2c4726d4fc05ccb32bf88ec6a6d66f3fd
Erik Hofmayer <ehofmayer@outlook.de>
2025-05-26 14:58:51 +0200

    Create git repos before running torte

--- src/main/resources/torte/fiasco-working-tree-kmax.sh
+++ src/main/resources/torte/fiasco-working-tree-kmax.sh
@@ -38,6 +38,9 @@ experiment-subjects() {
 
 experiment-stages() {
     push "$INPUT_DIRECTORY"/fiasco
+    if [ ! -d .git ]; then
+        git init
+    fi
     git add .
     if [ -n "$(git status --porcelain)" ]; then
         git commit -m "Automatic commit by Vari-Joern"
