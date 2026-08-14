9420a7fe070485bd9e4d0c001b00bf9580cd1486
Tim Bächle <t.baechle1999@googlemail.com>
2026-08-14 12:02:20 +0200

    Documentation additions to FixedSamplerConfig.java.

--- src/main/java/edu/kit/varijoern/samplers/other/FixedSamplerConfig.java
+++ src/main/java/edu/kit/varijoern/samplers/other/FixedSamplerConfig.java
@@ -17,7 +17,10 @@ import java.util.Objects;
  * Contains the configuration of the fixed sampler.
  */
 public class FixedSamplerConfig extends SamplerConfig {
+    // Literals used in the TOML file for the configuration of the fixed sampler.
     private static final String FEATURES_FIELD_NAME = "features";
+
+    // Fields for the configuration of the fixed sampler.
     private final @NotNull List<List<String>> sample;
 
     /**
@@ -29,30 +32,30 @@ public class FixedSamplerConfig extends SamplerConfig {
     public FixedSamplerConfig(@NotNull TomlTable toml) throws InvalidConfigException {
         super(toml);
         if (!toml.isArray(FEATURES_FIELD_NAME))
-            throw new InvalidConfigException("Features for fixed sampler are missing or not an array");
+            throw new InvalidConfigException("Features for fixed sampler are missing or not an array.");
         TomlArray configurationsTomlArray = Objects.requireNonNull(toml.getArray(FEATURES_FIELD_NAME));
         List<List<String>> configurations = new ArrayList<>();
+
         for (int i = 0; i < configurationsTomlArray.size(); i++) {
             TomlArray featuresTomlArray;
             try {
                 featuresTomlArray = configurationsTomlArray.getArray(i);
             } catch (TomlInvalidTypeException e) {
-                throw new InvalidConfigException("One of the configurations for the fixed sampler is not an array", e);
+                throw new InvalidConfigException("One of the configurations for the fixed sampler is not an array.", e);
             }
             List<String> features = new ArrayList<>();
             for (int j = 0; j < featuresTomlArray.size(); j++) {
                 try {
                     features.add(featuresTomlArray.getString(j));
                 } catch (TomlInvalidTypeException e) {
-                    throw new InvalidConfigException(
-                            "One of the features of configuration %d for the fixed sampler is not a string."
-                                    .formatted(i),
-                            e
+                    throw new InvalidConfigException(("One of the features of configuration %d for the fixed sampler " +
+                            "is not a string.").formatted(i), e
                     );
                 }
             }
             configurations.add(features);
         }
+
         this.sample = List.copyOf(configurations);
     }
 
@@ -64,8 +67,8 @@ public class FixedSamplerConfig extends SamplerConfig {
     /**
      * Returns the sample to be returned by the sampler as specified by the configuration file.
      *
-     * @return the features enabled in each of the configurations. One entry of the top-level list corresponds to one
-     * configuration.
+     * @return the features ({@link List} of {@link String}s) enabled in each of the configurations. One entry of the
+     * top-level {@link List} corresponds to one configuration.
      */
     public @NotNull List<List<String>> getSample() {
         return this.sample;
